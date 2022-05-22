20c5310d907a70331539859b4dd9550a102bba14
Eniwder <akabudou0120@yahoo.co.jp>
2022-05-23 00:09:06 +0900

    refactor main

--- main.js
+++ main.js
@@ -38,52 +38,65 @@ async function main(startTime) {
   const newPapers = papers
     .filter(_ => _.categories[0].term === 'cs.CL')
     .filter(_ => new Date(_.published) > startTime);
-  console.log(`[${newPapers.length}] new papers found`);
+  console.log(`[${newPapers.length}] new papers found at [${(new Date()).toLocaleString()}]`);
 
-  let tweetThreads = [];
+  const tweetThreads = [];
   for (let i = newPapers.length - 1; i >= 0; i--) {
     const paper = newPapers[i];
-    let { title, summary } = paper;
-    title = latexReplacer(title.replace(/[\n ]+/g, ' '));
-    summary = latexReplacer(summary.replace(/[\n ]+/g, ' '));
-    const doc = nlp.readDoc(paper.summary);
+    const { title, summary } = paper;
+    const escapedTitle = latexReplacer(title.replace(/[\n ]+/g, ' '));
+    const escapedSummary = latexReplacer(summary.replace(/[\n ]+/g, ' '));
+    const doc = nlp.readDoc(escapedSummary);
     const tokensWithTag = doc.tokens().out().zip(doc.tokens().out(its.pos))
-    const frequentPropn = tokensWithTag.filter(_ => _[1] === 'PROPN')
+    const frequentPropn = tokensWithTag
+      .filter(_ => _[1] === 'PROPN')
       .map(_ => _[0])
       .reduce((acc, v) => {
         acc[v] = (acc[v] || 0) + 1;
         return acc;
       }, {})
     const top4Propn = Object.entries(frequentPropn).sort((a, b) => b[1] - a[1]).map(_ => _[0]).slice(0, 4);
-    const summaryJP = (await axios.get(`https://script.google.com/macros/s/${process.env.Google_Token_Api}/exec?text=${encodeURI(summary)}&source=en&target=ja`).catch(e => {
-      console.log(e);
-      return { data: { text: null } }
-    })).data.text;
-    if (!summaryJP) return;
-    await createImage(summaryJP); // creat at [./media.png]
-    const mediaId = await TwitterClient.v1.uploadMedia('./media.png').catch(e => console.log(e));
-    if (mediaId) {
-      const text = `${title}\n${paper.id}\nkeywords: ${top4Propn.join(', ')}`;
-      tweetThreads.push({ text, media: { media_ids: [mediaId] }, reply_settings: "mentionedUsers" });
-      console.log(`pushed[${i}]: ${paper.id}`);
+    const transResult = await axios.get(`https://script.google.com/macros/s/${process.env.Google_Token_Api}/exec?text=${encodeURI(escapedSummary)}&source=en&target=ja`)
+      .catch(e => {
+        console.log(e);
+        return { data: { text: null } }
+      })
+    const summaryJP = transResult.data.text;
+
+    let mediaId = null;
+    if (summaryJP) {
+      await createImage(summaryJP); // creat at [./media.png]  
+      mediaId = await TwitterClient.v1.uploadMedia('./media.png').catch(e => console.log(e));
     } else {
-      console.log(`fail pushed[${i}]: ${paper.id}`);
+      console.log('summary JP not found');
+    }
+    if (mediaId) {
+      const text = `${escapedTitle}\n${paper.id}\nkeywords: ${top4Propn.join(', ')}`;
+      if (tweetThreads.length === 0) {
+        tweetThreads.push({ text, media: { media_ids: [mediaId] }, reply_settings: "mentionedUsers" });
+      } else {
+        tweetThreads.push({ text, media: { media_ids: [mediaId] } });
+      }
+      console.log(`pushed [${i}]: ${paper.id}`);
+    } else {
+      console.log(`failed push [${i}]: ${paper.id}`);
     }
     // Threadの最大数は25らしいので25ごとにツイート
     if (tweetThreads.length >= 25 || (tweetThreads.length > 0 && i === 0)) {
       await TwitterClient.v2.tweetThread(tweetThreads).catch(e => console.log(e));
-      tweetThreads = [];
+      console.log(`tweeted [${tweetThreads.length}]`);
+      tweetThreads.length = 0;
     }
-    await sleep(60 * 1000);
+    await sleep(30 * 1000);
   }
   return papers[0] ? new Date(papers[0].published) : startTime;
 }
 
 async function helper() {
-  let latestFeedTime = new Date(Date.now());
+  let latestFeedTime = new Date();
   let beLft = latestFeedTime;
   while (true) {
-    latestFeedTime = await main(latestFeedTime);
+    latestFeedTime = await main(latestFeedTime).catch(e => console.log(e));
     const sleetMinutes = (beLft === latestFeedTime) ? 15 : SearchMax;
     beLft = latestFeedTime;
     await sleep(sleetMinutes * 60 * 1000);
